b939a30107726a3a9dff18920c42c3e0152194f6
herangakr <ma.muthukuda@gmail.com>
2019-06-17 18:26:50 +0000

    Building a Form to Save New Places, added simple_form gem

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,9 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.all
+    @places = Place.order("name").page(params[:page]).per_page(5)
+  end
+
+  def new
+    @place = Place. new
   end
 end
